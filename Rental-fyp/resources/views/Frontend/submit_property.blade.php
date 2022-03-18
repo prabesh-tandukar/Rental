9c00dd6b623d2e7e03d608e5d60388791675394f
parabesh-sama <72059782+parabesh-sama@users.noreply.github.com>
2022-03-18 23:47:11 +0545

    front & form commit" ;

--- Rental-fyp/resources/views/Frontend/submit_property.blade.php
+++ Rental-fyp/resources/views/Frontend/submit_property.blade.php
@@ -14,7 +14,7 @@
       <form action="" method="POST" enctype="multipart/form-data">
         @csrf
 
-        <div class="form-form">
+        
           <div class="form-group">
             <label class="form-label" for="property_title">Property title:</label>
             <input type="text" class="form-control" name="property_title" id="property_tile" value="{{ old('property_tite') }}" placeholder="Enter Property Title">
@@ -23,72 +23,73 @@
           @endif
         </div>
         <div class="form-group">
-            <label for="address">Address:</label>
+            <label class="form-label" for="address">Address:</label>
             <input type="text" class="form-control" name="address" id="address" value="{{ old('address') }}" placeholder="Enter Address">
+            <select name="city" id="city">
+              <option value="">--Select-City--</option>
+              <option value="kathmandu">kathmandu</option>
+              <option value="lalitpur">lalitpur</option>
+              <option value="bhaktapur">bhaktapur</option>
+            </select>
           @if ($errors->has('address'))
           <div class="alert alert-danger">{{ $errors->first('address') }}</div>
           @endif
         </div>
-        </div>
        
         <div class="form-group">
-          <label for="Property Category">Property Category:</label>
-          <label for="name">Room:</label>
-          <input type="radio" class="form-control" name="property_category" id="property_category">
-          <label for="name">Flat:</label>
-          <input type="radio" class="form-control" name="property_category" id="property_category">
-          <label for="name">Apartment:</label>
-          <input type="radio" class="form-control" name="property_category" id="property_category">
-          <label for="name">Office Space:</label>
-          <input type="radio" class="form-control" name="property_category" id="property_category">
-          <label for="name">Business Sapce:</label>
-          <input type="radio" class="form-control" name="property_category" id="property_category">
-        @if ($errors->has('name'))
-        <div class="alert alert-danger">{{ $errors->first('name') }}</div>
-        @endif
-      </div>
+          <label for="property_category">Property Category:</label>
+            <input type="radio" class="form-control" name="property_category" id="room">
+            <label for="room">Room</label>
+            <input type="radio" class="form-control" name="property_category" id="flat">
+            <label for="flat">Flat</label>
+            <input type="radio" class="form-control" name="property_category" id="apartment">
+            <label for="apartment">Apartment</label>
+            <input type="radio" class="form-control" name="property_category" id="office_space">
+            <label for="office_space">Office Space</label>
+            <input type="radio" class="form-control" name="property_category" id="buisness_space">
+            <label for="business_space">Business Sapce</label>
+            
+            @if ($errors->has('name'))
+            <div class="alert alert-danger">{{ $errors->first('name') }}</div>
+            @endif
+        </div>
+
         <div class="form-group">
             <label for="road_size">Road Size:</label>
             <input type="text" class="form-control" name="road_size" id="road_size" value="{{ old('road_size') }}" placeholder="Enter Road Size">
-          @if ($errors->has('road_size'))
-          <div class="alert alert-danger">{{ $errors->first('road_size') }}</div>
-          @endif
+            <select name="road_size_unit" id="">
+              <option value="feet">feet</option>
+              <option value="meter">meter</option>
+            </select>
+            <select name="road_type" id="">
+              <option value="gravelled">Gravelled</option>
+              <option value="soil-stabilized">Soil Stabilized</option>
+              <option value="paved">Paved</option>
+              <option value="blacktopped">Blacktopped</option>
+              <option value="alley">Alley</option>
+            </select>
+            @if ($errors->has('road_size'))
+            <div class="alert alert-danger">{{ $errors->first('road_size') }}</div>
+            @endif
         </div>
        
         <div class="form-group">
-            <label for="name">Building Details :</label>
-            <input type="text" class="form-control" name="name" id="name" value="{{ old('name') }}" placeholder="Enter Property Title">
-          @if ($errors->has('name'))
-          <div class="alert alert-danger">{{ $errors->first('name') }}</div>
-          @endif
+          <label for="distance">Distance from main road:</label>
+          <input type="text" id="distance" name="distance">
+          <select name="road_size_unit" id="">
+            <option value="feet">feet</option>
+            <option value="meter">meter</option>
+          </select>
         </div>
+
         <div class="form-group">
-            <label for="name">Total Area:</label>
-            <input type="text" class="form-control" name="name" id="name" value="{{ old('name') }}" placeholder="Enter Property Title">
-          @if ($errors->has('name'))
-          <div class="alert alert-danger">{{ $errors->first('name') }}</div>
-          @endif
-        </div><div class="form-group">
-            <label for="name">Build Up Area:</label>
-            <input type="text" class="form-control" name="name" id="name" value="{{ old('name') }}" placeholder="Enter Property Title">
+            <label for="building_detail">Building Details :</label>
+            <input type="text" class="form-control" name="build_year" id="building_detail" value="{{ old('name') }}" placeholder="Enter Built Year">
           @if ($errors->has('name'))
           <div class="alert alert-danger">{{ $errors->first('name') }}</div>
           @endif
         </div>
-        <div class="form-group">
-            <label for="name">Property Face:</label>
-            <input type="text" class="form-control" name="name" id="name" value="{{ old('name') }}" placeholder="Enter Property Title">
-          @if ($errors->has('name'))
-          <div class="alert alert-danger">{{ $errors->first('name') }}</div>
-          @endif
-        </div>
-        <div class="form-group">
-            <label for="name">Property title:</label>
-            <input type="text" class="form-control" name="name" id="name" value="{{ old('name') }}" placeholder="Enter Property Title">
-          @if ($errors->has('name'))
-          <div class="alert alert-danger">{{ $errors->first('name') }}</div>
-          @endif
-        </div>
+
         <div class="form-group">
             <label for="name">Bedroom:</label>
             <input type="text" class="form-control" name="name" id="name" value="{{ old('name') }}" placeholder="Enter Property Title">
@@ -126,18 +127,29 @@
 </div>
 
 <div class="form-group">
-  <label for="name">Amenitites:</label>
-  <label for="Property Category">Property Category:</label>
-          <label for="name">Room:</label>
-          <input type="radio" class="form-control" name="property_category" id="property_category">
-          <label for="name">Flat:</label>
-          <input type="radio" class="form-control" name="property_category" id="property_category">
-          <label for="name">Apartment:</label>
-          <input type="radio" class="form-control" name="property_category" id="property_category">
-          <label for="name">Office Space:</label>
-          <input type="radio" class="form-control" name="property_category" id="property_category">
-          <label for="name">Business Sapce:</label>
-          <input type="radio" class="form-control" name="property_category" id="property_category">
+  <label for="amenities">Amenitites:</label>
+          <label for="wifi">Wifi:</label>
+          <input type="radio" class="form-control" name="wifi" id="wifi">
+          <label for="security">Security:</label>
+          <input type="radio" class="form-control" name="security" id="security">
+          <label for="air_conditioner">Air Conditioner:</label>
+          <input type="radio" class="form-control" name="air_conditioner" id="air_conditioner">
+          <label for="water_supply">Water Supply:</label>
+          <input type="radio" class="form-control" name="water_supply" id="water_supply">
+          <label for="balcony">Balcony:</label>
+          <input type="radio" class="form-control" name="balcony" id="balcony">
+          <label for="gym">Gym:</label>
+          <input type="radio" class="form-control" name="gym" id="gym">
+          <label for="swimming_pool">Swimming Pool:</label>
+          <input type="radio" class="form-control" name="swimming_pool" id="swimming_pool">
+          <label for="tv_cable">Tv Cable:</label>
+          <input type="radio" class="form-control" name="tv_cable" id="tv_cable">
+          <label for="laundry">Laundry:</label>
+          <input type="radio" class="form-control" name="laundry" id="laundry">
+          <label for="lift">Lift:</label>
+          <input type="radio" class="form-control" name="lift" id="lift">
+          <label for="solar">Solar:</label>
+          <input type="radio" class="form-control" name="solar" id="solar">
   
 @if ($errors->has('name'))
 <div class="alert alert-danger">{{ $errors->first('name') }}</div>
@@ -153,6 +165,44 @@
 @endif
 </div>
 
+<div>
+    <label for="price">Price</label>
+    <input type="text" name="price" id="price" placeholder="Rs">
+    <select name="price" id="price">
+      <option value=""></option>
+      <option value="per_month">Per Month</option>
+      <option value="per_annum">Per Annum</option>
+    </select>
+</div>
+
+<div>
+  <label for="negotiable">Negotiable</label>
+    <label for="yes">Yes</label>
+    <input type="radio" name="yes" id="yes">
+    <label for="no">No</label>
+    <input type="radio" name="no" id="no">
+
+</div>
+
+<div>
+  <label for="owner">Owner Information</label>
+  <input type="text" name="owner_name" id="owner" placeholder="Enter Owner Name">
+  <input type="text" name="owner_email" id="owner" placeholder="Enter Owner Email">
+  <input type="text" name="owner_phone" id="owner" placeholder="Enter Owner Phone">
+</div>
+
+<div>
+  <label for="location">Location</label>
+    <label for="yes">Yes</label>
+    <input type="radio" name="yes" id="yes">
+    <label for="no">No</label>
+    <input type="radio" name="no" id="no">
+</div>
+
+<div>
+  <label for="upload_image">Upload Image</label>
+  <input type="file">
+</div>
 <div>
   <button>Submit</button>
 </div>
